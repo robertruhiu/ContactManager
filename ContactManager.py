b7bc85991a60d793a9bf02e88395518eead414d2
robert ruhiu <robertruhiu@gmail.com>
2017-09-18 15:25:08 +0000

    ContactManager with user input

---
+++ ContactManager.py
@@ -0,0 +1,20 @@
+class ContactManager:
+    def __init__(self, name, phone_number, gender, email_address, postal_address):
+        self.name = name
+        self.phone_number = phone_number
+        self.gender = gender
+        self.email_address = email_address
+        self.postal_address = postal_address
+
+    def contact_info(self):
+        return self.name, self.phone_number, self.gender, self.email_address, self.postal_address
+
+
+name = input("What is your name:")
+phone_number = int(input("What is your phone number:"))
+gender = input("What is your gender:")
+email_address = input("What is your email address:")
+postal_address = input("What is your postal address:")
+
+newcontact = ContactManager(name, phone_number, gender, email_address, postal_address)
+print(newcontact.contact_info())
