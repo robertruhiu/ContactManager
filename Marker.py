f66f8a10c3e0ad8a5ec1982fb01e4796b90a0c82
robert ruhiu <robertruhiu@gmail.com>
2017-09-19 08:58:34 +0000

    marker example for oop

--- Marker.py
+++ Marker.py
@@ -1,13 +1,17 @@
 class Marker:
-    def __init__(self, color="Red"):
+    def __init__(self, color = "Red"):
         self.color = color
 
 
 class MarkerBox:
-    def __init__(self, markers=[]):
+    # noinspection PyShadowingNames
+    def __init__(self, markers = None):  # markers list is initiated here
+        if markers is None:
+            markers = []
         self.markers = markers
 
     def add_marker(self, marker):
+
         self.markers.append(marker)
 
     def remove_marker(self, color):
@@ -16,8 +20,17 @@ class MarkerBox:
                 self.markers.remove(marker)
                 return marker
             return None
-        if __name__ == "__main":
-            mest_markers = MarkerBox()
-            mest_markers.add_marker(Marker(color="blue"))
-            mest_markers.add_marker(Marker(color="black"))
-            mest_markers.remove_marker("blue")
+
+
+if __name__ == "__main__":
+
+    mest_markers = MarkerBox()
+    mest_markers.add_marker(Marker(color = "blue"))
+    mest_markers.add_marker(Marker(color = "black"))
+    mest_markers.add_marker(Marker())  # fetches the default variable of red
+    mest_markers.remove_marker("blue")
+
+    for markers in mest_markers.markers:  # the .markers is the list we are accessing
+        print(markers.color)
+
+    print("The marker box has {} markers".format(len(mest_markers.markers)))
