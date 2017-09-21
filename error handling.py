f5f323811073d3632e0f30c8e4132c3228d48f0d
robert ruhiu <robertruhiu@gmail.com>
2017-09-21 09:46:36 +0000

    __str__ function to enable to overide output to specified

--- error handling.py
+++ error handling.py
@@ -1,29 +1,25 @@
 class Fellows:
-    fellows_created=0
+    fellows_created = 0                 #static field
 
-    def __init__(self,name,nationality):
-        self.name=name
-        self.nationality=nationality
-        if Fellows.fellows_created==4:
-            raise Exception("we cannot afford to hire {} from {}".format(name,nationality))
-        Fellows.fellows_created+=1
+    def __init__ (self, name, nationality):             #class calls this constructor to initiate instances to be used by the object
+        if Fellows.fellows_created == 4:                #condition to be met to raise error
+            raise Exception("we cannot afford to pay this fellow {} from {}".format(name, nationality))
+        else:
+            self.name = name
+            self.nationality = nationality
+            Fellows.fellows_created += 1                #counter to enable the loop
 
-    def add_fellows(self,name,nationality):
-        self.name=input("enter your name:")
-        self.nationality=input("enter your country:")
+    def __str__ (self):                                         #overides string representation of the class
+        return "{}, {}".format(self.name, self.nationality)
 
 
-fellow1=Fellows("dennis","usa")
+fellow1 = Fellows("Pascal", "Congo")
 print(fellow1)
-
-fellow2=Fellows("dennis","usa")
+fellow2 = Fellows("Francis", "Ghana")
 print(fellow2)
-
-fellow3=Fellows("dennis","usa")
+fellow3 = Fellows("Andrew", "USA")
 print(fellow3)
-
-fellow4=Fellows("dennis","usa")
+fellow4 = Fellows("Simphiwe", "South africa")
 print(fellow4)
-
-fellow5=Fellows("dennis","usa")
+fellow5 = Fellows("Dennis", "Mongolio")
 print(fellow5)
